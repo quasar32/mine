9bc2ed6b4aef58f2c2bd4cd0eccd176a74eaf8f7
quasar32 <fontejam@tamu.edu>
2024-06-23 19:32:24 -0500

    remove exrc

---
+++ .ycm_extra_conf.py
@@ -0,0 +1,9 @@
+def Settings( **kwargs ):
+  return {
+    'flags': [
+        '-Ilib/glad/include', 
+        '-Ilib/stb', 
+        '-Ilib/glfw/include', 
+        '-Ilib/cglm/include'
+    ]
+  }
